c81251e00b453239330815188e3fbcf3a6cecbc6
Himan Dhawan <hdhawan@paymentus.com>
2021-10-26 16:36:26 +0530

    Added Numbers Project

---
+++ Numbers/Numbers.h
@@ -0,0 +1,18 @@
+//
+//  Numbers.h
+//  Numbers
+//
+//  Created by Himan Dhawan on 26/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Numbers.
+FOUNDATION_EXPORT double NumbersVersionNumber;
+
+//! Project version string for Numbers.
+FOUNDATION_EXPORT const unsigned char NumbersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Numbers/PublicHeader.h>
+
+
---
+++ Numbers/Numbers.swift
@@ -0,0 +1,18 @@
+//
+//  Numbers.swift
+//  Numbers
+//
+//  Created by Himan Dhawan on 26/10/21.
+//
+
+import Foundation
+
+public class Numbers {
+    let firstNumber : Int
+    let secondNumber : Int
+    
+    public init(firstNumber : Int, secondNumber : Int) {
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+    }
+}
